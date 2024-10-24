f1c788e8ada4cc9fe4501fb91f21ce980aa9fb2f
AliAlTaweel <engalihaitham@gmail.com>
2024-10-24 04:09:17 +0300

    Update app.ts
    Port number corrected

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.register(itemMaker);
 const start = async () => {
   try {
     await app.listen({ port: 3001 });
-    app.log.info(`Server running at http://localhost:3000`);
+    app.log.info(`Server running at http://localhost:3001`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
